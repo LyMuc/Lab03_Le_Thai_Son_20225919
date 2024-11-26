d9a5bdfc9286827b2bc1a31a02931b4ef4e06336
Le Thai Son 20225919 <lethaison19122004@gmail.com>
2024-11-26 16:29:04 +0700

    Bai 5

--- Src/DigitalVideoDisc.java
+++ Src/DigitalVideoDisc.java
@@ -1,10 +1,14 @@
 
 public class DigitalVideoDisc {
+	
+	private static int nbDigitalVideoDiscs =0;
 	private String title;
 	private String category;
 	private String director;
 	private int length;
 	private float cost;
+	private int id;
+	
 	public String getTitle() {
 		return title;
 	}
@@ -23,12 +27,18 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -37,6 +47,9 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -44,6 +57,15 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getId() {
+		return id;
 	}
 	
 }
